7b883fe5244a6f8444588e22661bfbd77d991dc5
Leonardo Almeida <leocalm@gmail.com>
2025-11-20 14:04:29 +0100

    Updating the script

--- migrations/0002_account.sql
+++ migrations/0002_account.sql
@@ -5,47 +5,42 @@ CREATE TYPE account_type AS ENUM (
     'Checking',
     'Savings',
     'CreditCard'
-);
+    );
 
 -- 2) Currency table
-CREATE TABLE currency (
-                          id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-                          name TEXT NOT NULL,
-                          symbol TEXT NOT NULL,
-                          currency TEXT NOT NULL,
-                          decimal_places SMALLINT NOT NULL,
-                          created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-                          updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+CREATE TABLE currency
+(
+    id             UUID PRIMARY KEY     DEFAULT gen_random_uuid(),
+    name           TEXT        NOT NULL,
+    symbol         TEXT        NOT NULL,
+    currency       TEXT        NOT NULL,
+    decimal_places SMALLINT    NOT NULL,
+    created_at     TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at     TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 -- 3) Account table
-CREATE TABLE account (
-                         id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-                         name TEXT NOT NULL,
-                         color TEXT NOT NULL,
-                         icon TEXT NOT NULL,
-                         account_type account_type NOT NULL,
-                         currency_id UUID NOT NULL REFERENCES currency(id) ON DELETE RESTRICT,
-                         balance BIGINT NOT NULL,
-                         created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-                         updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+CREATE TABLE account
+(
+    id           UUID PRIMARY KEY      DEFAULT gen_random_uuid(),
+    name         TEXT         NOT NULL,
+    color        TEXT         NOT NULL,
+    icon         TEXT         NOT NULL,
+    account_type account_type NOT NULL,
+    currency_id  UUID         NOT NULL REFERENCES currency (id) ON DELETE RESTRICT,
+    balance      BIGINT       NOT NULL,
+    created_at   TIMESTAMPTZ  NOT NULL DEFAULT now(),
+    updated_at   TIMESTAMPTZ  NOT NULL DEFAULT now()
 );
 
--- 4) updated_at trigger
-CREATE OR REPLACE FUNCTION set_updated_at()
-RETURNS TRIGGER AS $$
-BEGIN
-    NEW.updated_at = now();
-RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
 CREATE TRIGGER set_currency_updated_at
-    BEFORE UPDATE ON currency
+    BEFORE UPDATE
+    ON currency
     FOR EACH ROW
-    EXECUTE FUNCTION set_updated_at();
+EXECUTE FUNCTION set_updated_at();
 
 CREATE TRIGGER set_account_updated_at
-    BEFORE UPDATE ON account
+    BEFORE UPDATE
+    ON account
     FOR EACH ROW
-    EXECUTE FUNCTION set_updated_at();
+EXECUTE FUNCTION set_updated_at();
